2d732bbca68f6aec01708a1925e4e3ea272ba5df
Kuhn <KALEE_KUHN@homedepot.com>
2017-05-23 13:28:54 -0500

    second basic commit

--- app.js
+++ app.js
@@ -22,9 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.get('/', index);
 app.use('/users', users);
 
+app.get('/health', function (req, res, next) {
+  res.json({"health":"OK"});
+  //res.status(200).end();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
